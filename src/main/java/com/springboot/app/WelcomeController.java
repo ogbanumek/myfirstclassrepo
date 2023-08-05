fe278a4a70d15cf47d64437cd71b558172feb41b
ogbanumek <ogbanumek@yahoo.com>
2023-08-05 16:04:36 -0400

    Update WelcomeController.java

--- src/main/java/com/springboot/app/WelcomeController.java
+++ src/main/java/com/springboot/app/WelcomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
-	@GetMapping("/welcome")
+	@GetMapping("/devops")
 	public String welcome() {
 		return "welcome to spring boot app development";
 	}
